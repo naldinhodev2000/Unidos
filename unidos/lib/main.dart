574d7b76e0f6d02b857f3955cda18b2e037e7a06
Naldinho <ffranciscoafarias@gmail.com>
2026-07-29 19:58:08 -0300

    adicionando icone

--- unidos/lib/main.dart
+++ unidos/lib/main.dart
@@ -1,16 +1,18 @@
+import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
 import 'package:unidos/telas/main_page.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // Inicializa o SQLite para Linux/Desktop
-  sqfliteFfiInit();
-
-  // Diz ao sqflite para usar a implementação FFI
-  databaseFactory = databaseFactoryFfi;
+  // Usa SQLite FFI apenas no Linux/Desktop
+  if (Platform.isLinux || Platform.isWindows || Platform.isMacOS) {
+    sqfliteFfiInit();
+    databaseFactory = databaseFactoryFfi;
+  }
 
   runApp(const UnidosApp());
 }
